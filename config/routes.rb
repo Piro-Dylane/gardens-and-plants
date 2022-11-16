979aa24a7c5159ceef5e41bdb48ac896546e4388
Dylane Piro <piro.dylan@outlook.fr>
2022-11-16 18:03:31 +0100

    rails new

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :gardens do
+    resources :plants, only: [:new, :create]
+  end
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
